409ac0f39723c6bb758ee844e466f56131137766
khatiwadaprajwal <prajwalkhatiwada28@gmail.com>
2026-01-10 14:16:32 +0545

    ci setup via github actions

--- Event_Backend/src/app.ts
+++ Event_Backend/src/app.ts
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser()); 
 const allowedOrigins = [
   "http://localhost:5173", 
-  "http://localhost:5173",
+  "http://localhost:5174",
 
   env.CLIENT_URL          
 ];
